32183df64e725ce1fdf62ada69ae7d53e4507bb7
Khang <h.v.khang68@gmail.com>
2021-12-12 22:35:11 +0700

    first commit

---
+++ app/src/main/java/com/androiddevs/SimpleNewsApp/api/NewsAPI.kt
@@ -0,0 +1,17 @@
+package com.androiddevs.mvvmnewsapp.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    )
+}
---
+++ app/src/main/java/com/androiddevs/SimpleNewsApp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = ""
+    }
+}
